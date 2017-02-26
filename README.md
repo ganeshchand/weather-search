c9337ae57e7b56fdc9e707ee2880fe93e6a88fb3
ganeshchand <ganeshchand@gmail.com>
2017-02-26 01:25:03 -0800

    Added README.md and HTML content using scalatags

---
+++ README.md
@@ -0,0 +1,13 @@
+# Weather search webapp using ScalaJS
+
+##How to run this app:
+
+* clone this repository
+* `cd` to project directory
+* Run the following sbt commands:
+```
+> sbt
+> ~fastOpsJS
+
+```
+* Open a browser and type: http://localhost:63342/weather-search/index-dev.html
--- src/main/scala/com/gc/learning/scala/scalajs/app/webapp/WeatherSearch.scala
+++ src/main/scala/com/gc/learning/scala/scalajs/app/webapp/WeatherSearch.scala
@@ -1,5 +1,8 @@
 package com.gc.learning.scala.scalajs.app.webapp
 
+import org.scalajs.dom.html
+import scalatags.JsDom.all._
+
 import scala.scalajs.js.annotation.JSExport
 
 /**
@@ -8,7 +11,15 @@ import scala.scalajs.js.annotation.JSExport
 @JSExport
 object WeatherSearch{
   @JSExport
-  def main(): Unit = {
-    println("Hello ScalaJS Programmer!")
+  def main(target: html.Div): Unit = {
+    val myFavoriteCities = List("San Francisco", "Kathmandu", "Bangalore", "Tokyo", "New York")
+    target.appendChild(
+      div (
+        h1("Weather Search"),
+        h3("My favorite cities:"),
+          myFavoriteCities.map {city => li(city)}
+      ).render
+    )
+
   }
 }
